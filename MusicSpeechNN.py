3e96fdd11e670655205db2a5f594c16a7e0d2f61
Manpreet Bahl <manpreetsingh.bahl@gmail.com>
2019-03-14 19:08:57 -0700

    Added random training and test selection

--- MusicSpeechNN.py
+++ MusicSpeechNN.py
@@ -1,15 +1,20 @@
 import os
+import sys
+import random
 import numpy as np
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 from keras import layers
 from keras.models import Sequential
-from keras.layers.recurrent import LSTM, SimpleRNN
+from keras.layers.recurrent import LSTM
 from keras.layers import Dense
 from keras import optimizers
 
 class Dataset:
+    """
+    This class organizes the GTZAN dataset into training and testing data
+    """
     def __init__(self):
         self.train_data = [] 
         self.train_label = []
@@ -20,14 +25,12 @@ class Dataset:
         speech_folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'music_speech/speech_wav')
         music_folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'music_speech/music_wav')
 
-        #speech_sum = sum([ord(c) for c in "Speech"])
-        #music_sum = sum([ord(c) for c in "Music"])
-        speech_sum = 0
-        music_sum = 1
+        speech_files = self.shuffle_audio_files(folder_path=speech_folder_path)
+        music_files = self.shuffle_audio_files(folder_path=music_folder_path)
 
         file_num = 48
         count = 0
-        for speech_file in os.listdir(speech_folder_path):
+        for speech_file in speech_files:
             clip, sr = librosa.load(os.path.join(speech_folder_path, speech_file))
             if count < file_num:
                 self.train_data.append(librosa.feature.melspectrogram(y=clip, sr=sr))
@@ -38,7 +41,7 @@ class Dataset:
             count += 1
         
         count = 0
-        for music_file in os.listdir(music_folder_path):
+        for music_file in music_files:
             clip, sr = librosa.load(os.path.join(music_folder_path, music_file))
             if count < file_num:
                 self.train_data.append(librosa.feature.melspectrogram(y=clip, sr=sr))
@@ -53,15 +56,27 @@ class Dataset:
 
         self.test_data = np.array(self.test_data)
         self.test_label = np.array(self.test_label)
+    
+    def shuffle_audio_files(self, folder_path=None):
+        audio_files = []
+        if os.path.isdir(folder_path):
+            for audio_file in os.listdir(folder_path):
+                audio_files.append(audio_file)
+        else:
+            print ("Directory " + str(folder_path) + " does not exist")
+            sys.exit(1)
+
+        random.shuffle(audio_files)
+        return audio_files
+
 
 ds = Dataset()
 print(ds.train_data.shape)
 print(ds.train_label.shape)
 
-#opt = optimizers.SGD(lr=0.5, momentum=0.5, decay=0.0, nesterov=False)
 opt = optimizers.Adam()
-batch_size = 1
-nb_epochs = 50
+batch_size = 3
+nb_epochs = 15
 
 print('Build LSTM RNN model ...')
 model = Sequential()
@@ -80,58 +95,3 @@ print("\nTesting ...")
 score, accuracy = model.evaluate(x=ds.test_data, y=ds.test_label, batch_size=batch_size, verbose=1)
 print("Test loss:  ", score)
 print("Test accuracy:  ", accuracy)
-
-#print(model.predict(x=ds.test_data, batch_size=1))
-"""
-def read_data(folder_path):
-    #Reads .wav files from folder_path into a data array
-    data = []
-    for filename in os.listdir(folder_path):
-        filepath = os.path.join(folder_path, filename)
-        clip, sr = librosa.load(filepath)
-        data.append(librosa.feature.melspectrogram(y=clip, sr=sr))
-    return np.array(data)
-
-print("\nLoading .wav files")
-speech_folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'music_speech/speech_wav')
-music_folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'music_speech/music_wav')
-
-speech_data = read_data(speech_folder_path)
-music_data = read_data(music_folder_path)
-print("Finished Loading")
-f_data = np.concatenate((speech_data, music_data))
-print(f_data.shape)
-
-
-#print(len(f_data[0]))
-#print(len(f_data[-1]))
-
-#print(f_data)
-#print(len(f_data))
-
-
-plt.figure(figsize=(10, 4))
-librosa.display.specshow(librosa.power_to_db(f_data[-1], ref=np.max))
-plt.colorbar(format='%+2.0f dB')
-plt.title('Mel spectrogram')
-plt.tight_layout()
-plt.savefig("test_1.png")
-"""
-
-"""
-opt = Adam()
-
-print('Build LSTM RNN model ...')
-model = Sequential()
-model.add(LSTM(units=128, dropout=0.05, recurrent_dropout=0.35, return_sequences=True, input_shape=f_data[0].shape))
-model.add(LSTM(units=32, dropout=0.05, recurrent_dropout=0.35, return_sequences=False))
-model.add(Dense(units=2, activation='softmax'))
-
-
-print("Compiling ...")
-model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-model.summary()
-
-print("Training ...")
-model.fit(genre_features.train_X, genre_features.train_Y, batch_size=batch_size, epochs=nb_epochs, callbacks= [TensorBoard(log_dir='./temp/log')])
-"""
